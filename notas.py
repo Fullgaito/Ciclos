82fd287439235d33f9703a1a4005a23cfc9727b8
Sergio Gaitan <104282835+Fullgaito@users.noreply.github.com>
2022-05-14 16:48:40 -0500

    Update notas.py

--- notas.py
+++ notas.py
@@ -4,7 +4,7 @@ aprobados=0 #contador para las notas aprobadas
 for i in range(1,nEstudiantes+1,1): #como se conoce la cantidad de estudiantes se implementa un for para ejecutar una serie de pasos cuando se conoce el fin del ciclo
     nota=float(input(f"Ingrese la nota del estudiante {i}: ")) #se pide las notas
     while nota<0 or nota>5: #se valida que las notas sean correctas
-        nota=int(input("Por favor ingrese una nota entre 0 y 5: ")) #se vuelve a pedir la nota en caso de que esta no sea correcta
+        nota=float(input("Por favor ingrese una nota entre 0 y 5: ")) #se vuelve a pedir la nota en caso de que esta no sea correcta
     if nota>=0 and nota<=2.9: #se usa un condicional para indicar cuando una nota es desaprobada
         print("La nota es no aprobada")
         desaprobados+=1 #se incrementa el contador en 1 en caso de que esa nota no sea aprobada
